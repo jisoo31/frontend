4bab4a6987cdf7a17f95d054c2462021baa7e1b9
jisoo31 <jisooworkpla@gmail.com>
2025-10-15 02:58:28 +0900

    1014

---
+++ 1014/다소니/js/main.js
@@ -0,0 +1,31 @@
+$(function(){
+
+    
+    setTimeout(function(){
+        $('.slider li .text0').addClass('on');
+        $('.slider li .atext0').addClass('on');
+    },500);
+
+
+    let bx = $('.slider').bxSlider({
+        auto:true,
+        controls:false,
+        pager:false,
+        mode:"fade",
+        pause:5000,
+        onSlideBefore:function(){},
+        onSlideAfter:function(){
+
+
+            let k = bx.getCurrentSlide(); //현재 슬라이드 번호
+            $('.slider li').find('h2').removeClass('on');
+            $('.slider li').find('p').removeClass('on');
+            $('.slider li .text'+k).addClass('on');
+            $('.slider li .atext'+k).addClass('on');
+            
+        
+        }
+    });
+
+
+});
